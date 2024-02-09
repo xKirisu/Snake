99cc20daba32215769c733dba7577039615e0411
Kirisu <xkrystianopalacz@gmail.com>
2024-02-09 19:56:18 +0100

    UI and other

--- Snake/Snake.cpp
+++ Snake/Snake.cpp
@@ -2,8 +2,11 @@
 #include <SFML/Graphics.hpp>
 #include <fstream>
 #include <algorithm> 
+#include <map>
 #define BlockSize 64
-#define UISize 8
+#define WindowSizeX 832
+#define WindowSizeY 896
+#define UISize WindowSizeY - WindowSizeX
 
 enum Direction {
     up, down, left, right
@@ -11,39 +14,63 @@ enum Direction {
 std::string LoadHighScore(std::string path);
 int WinMain()
 {
-    std::string highscore = LoadHighScore("save.dat");
-    int hightscore_int;
-    hightscore_int = std::stoi(highscore);
+   /* std::string highscore = LoadHighScore("save.dat");
+    int hightscore_int = 3;*/
+    //hightscore_int = std::stoi(highscore);
 
-
-    sf::RenderWindow window(sf::VideoMode(832, 840), "Snake");
+    bool isPlay = true;
+    sf::RenderWindow window(sf::VideoMode(WindowSizeX, WindowSizeY), "Snake");
 
     //Random
     std::srand(time(NULL));
     uint8_t randX = std::rand() % 13;
     uint8_t randY = std::rand() % 13;
 
+    //UI Panel
+    sf::RectangleShape uibakground;
+    uibakground.setFillColor(sf::Color::Cyan);
+    uibakground.setSize(sf::Vector2f(WindowSizeX, UISize));
+    uibakground.setPosition(0, WindowSizeY-BlockSize);
+
+    //Reset Button
+    sf::RectangleShape reset_button;
+
     //Text
     sf::Font font;
-    if (font.loadFromFile("font/Roboto_Bold.ttf")) {
-        return 1;
-    }
-    sf::Text score_value_text;
-    sf::Text hightscore_value_text;
-    sf::Text score_text;
-    sf::Text hisghscore_text;
+    font.loadFromFile("font/Roboto-Black.ttf");
 
-    score_value_text.setFont(font);
-    hightscore_value_text.setFont(font);
-    score_text.setFont(font);
-    hisghscore_text.setFont(font);
+    sf::Text text_score_value, text_score, text_highscore, text_highscore_value, text_gameover;
 
+        text_gameover.setFont(font);
+        text_gameover.setString("Game Over");
+        text_gameover.setCharacterSize(96);
+        text_gameover.setPosition(WindowSizeX / 4 - BlockSize/3, WindowSizeY / 3);
+        
+        text_score_value.setFont(font);
+        text_score_value.setCharacterSize(42);
+        text_score_value.setPosition(WindowSizeX - BlockSize*1.85, WindowSizeY - BlockSize + 2);
+
+        text_score.setFont(font);
+        text_score.setString("Score:");
+        text_score.setPosition(WindowSizeX - (BlockSize * 3.9), WindowSizeY - BlockSize);
+        text_score.setCharacterSize(42);
+
+        text_highscore.setFont(font);
+        text_highscore.setString("Highscore:");
+        text_highscore.setCharacterSize(42);
+        text_highscore.setPosition(BlockSize * 1.2, WindowSizeY - BlockSize + 2);
+
+        text_highscore_value.setFont(font);
+        text_highscore_value.setCharacterSize(42);
+        text_highscore_value.setPosition(BlockSize * 4.55, WindowSizeY - BlockSize + 4);
+
+        text_highscore_value.setString("123");
     //Head
     sf::RectangleShape head;
     head.setFillColor(sf::Color::Yellow);
-    Direction direction;
     head.setPosition(448, 448);
     head.setSize(sf::Vector2f(BlockSize, BlockSize));
+    Direction direction = down;
 
     //Food
     sf::CircleShape food(BlockSize/2);
@@ -59,17 +86,15 @@ int WinMain()
 
     std::vector<sf::Vector2f> tail{ head.getPosition(),head.getPosition(), head.getPosition() };
 
-
-
     for (sf::Uint8 i = 0; i < 3; i++) {
         tail[i].y += -BlockSize-BlockSize*i;
     }
 
-
-
+    //Score values
+    text_score_value.setString(std::to_string(tail_counter - 3));
 
     sf::Clock time;
-    float getTime;
+    float getTime = 0;
     while (window.isOpen())
     {
         sf::Event event;
@@ -79,10 +104,10 @@ int WinMain()
                 window.close();
 
             if (event.type == sf::Event::KeyPressed) {
-                if (event.key.code == sf::Keyboard::W) direction = up;
-                if (event.key.code == sf::Keyboard::S) direction = down;
-                if (event.key.code == sf::Keyboard::A) direction = left;
-                if (event.key.code == sf::Keyboard::D) direction = right;
+                if (event.key.code == sf::Keyboard::W) if (direction != down)   direction = up;
+                if (event.key.code == sf::Keyboard::S) if (direction != up)     direction = down;
+                if (event.key.code == sf::Keyboard::A) if (direction != right)  direction = left;
+                if (event.key.code == sf::Keyboard::D) if (direction != left)   direction = right;
             }
         }
 
@@ -90,7 +115,7 @@ int WinMain()
        
         window.clear();
 
-        if (getTime > 0.5f) {
+        if (getTime > 0.5f&&isPlay) {
 
             sf::Vector2f last_position = head.getPosition();
 
@@ -108,13 +133,40 @@ int WinMain()
                 head.move(BlockSize, 0);
                 break;
             }
+            //Check if head go out of borders
+            sf::Vector2f checkhead = head.getPosition();
+            if (checkhead.x < 0 || checkhead.y < 0 || checkhead.x > WindowSizeX - BlockSize || checkhead.y > WindowSizeY - (BlockSize+UISize)) {
+                isPlay = false;
+            }
 
+            //Check if tail collided head
+            for (sf::Vector2f tailpos : tail) {
+                if (head.getGlobalBounds().contains(tailpos)) {
+                    isPlay = false;
+                }
+            }
+
+            //Ckeck if food iseatable
             if (head.getGlobalBounds().contains(food.getPosition())) {
+
                 tail_counter++;
                 tail.push_back(head.getPosition());
+
                 randX = std::rand() % 13;
                 randY = std::rand() % 13;
                 food.setPosition(randX * BlockSize, randY * BlockSize);
+
+                bool checkagain = true;
+                for (int i = 0; i < tail_counter; i++) {
+                    if (food.getGlobalBounds().contains(tail[i])) {
+                        randX = std::rand() % 13;
+                        randY = std::rand() % 13;
+                        food.setPosition(randX * BlockSize, randY * BlockSize);
+                        i = 0;
+                    }
+                }
+                
+                text_score_value.setString(std::to_string(tail_counter - 3));
             }
 
             tail[last_id] = last_position;
@@ -127,14 +179,31 @@ int WinMain()
 
         }
 
-        window.draw(food);
-
+        //Draw tail
         for (int i = 0; i < tail_counter; i++) {
             tail_draw.setPosition(tail[i]);
             window.draw(tail_draw);
         }
 
+        //Drawfood
+        window.draw(food);
+        
+
+        //Drawhead
         window.draw(head);
+
+        if (!isPlay) {
+            window.draw(text_gameover);
+            window.draw(reset_button);
+        }
+
+        //DrawUI
+        window.draw(uibakground);
+        window.draw(text_highscore);
+        window.draw(text_highscore_value);
+        window.draw(text_score);
+        window.draw(text_score_value);
+
         window.display();
         
     }
